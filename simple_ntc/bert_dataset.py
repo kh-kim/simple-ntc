a35b262a5317e3b07d7f859a691eed379705b576
khkim <pointzz.ki@gmail.com>
2021-02-07 21:33:01 +0900

    split data_loader

---
+++ simple_ntc/bert_dataset.py
@@ -0,0 +1,51 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class TextClassificationCollator():
+
+    def __init__(self, tokenizer, max_length, with_text=True):
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+        self.with_text = with_text
+
+    def __call__(self, samples):
+        texts = [s['text'] for s in samples]
+        labels = [s['label'] for s in samples]
+
+        encoding = self.tokenizer(
+            texts,
+            padding=True,
+            truncation=True,
+            return_tensors="pt",
+            max_length=self.max_length
+        )
+
+        return_value = {
+            'input_ids': encoding['input_ids'],
+            'attention_mask': encoding['attention_mask'],
+            'labels': torch.tensor(labels, dtype=torch.long),
+        }
+        if self.with_text:
+            return_value['text'] = texts
+
+        return return_value
+
+
+class TextClassificationDataset(Dataset):
+
+    def __init__(self, texts, labels):
+        self.texts = texts
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.texts)
+    
+    def __getitem__(self, item):
+        text = str(self.texts[item])
+        label = self.labels[item]
+
+        return {
+            'text': text,
+            'label': label,
+        }
