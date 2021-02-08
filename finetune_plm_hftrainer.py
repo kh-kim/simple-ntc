7c388f32de4833400037e2c1cbac4d57d44e707d
khkim <pointzz.ki@gmail.com>
2021-02-08 11:09:25 +0900

    minor

--- finetune_plm_hftrainer.py
+++ finetune_plm_hftrainer.py
@@ -22,10 +22,12 @@ def define_argparser():
     p.add_argument('--train_fn', required=True)
     p.add_argument('--pretrained_model_name', type=str, default='beomi/kcbert-base')
 
+    p.add_argument('--valid_ratio', type=float, default=.2)
     p.add_argument('--batch_size_per_device', type=int, default=32)
     p.add_argument('--n_epochs', type=int, default=5)
+
     p.add_argument('--warmup_ratio', type=float, default=.2)
-    p.add_argument('--valid_ratio', type=float, default=.2)
+    p.add_argument('--use_radam', action='store_true')
 
     p.add_argument('--max_length', type=int, default=100)
 
